b59dd50b688f50fa1af8f18a8dc69cae290f56d7
wanglei.breath <wanglei.breath@bytedance.com>
2025-03-06 16:01:34 +0800

    chore: add new TypeScript file and update layout component
    - Added a new TypeScript file `hello.ts` with a simple console log statement.
    - Updated the `layout.tsx` component to include an additional `<h1>` element.
    - Removed an unused image file `image.png`.
    - Updated binary Excel files with minor changes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+d
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <h1>hello world</h1>
     </html>
   );
 }
